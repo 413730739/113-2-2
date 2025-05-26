e10c7cb57a1912563ed221572c2b3b8b2dc085a8
413730739 <413730739@o365.tku.edu.tw>
2025-05-27 01:13:55 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,18 +1,18 @@
-// Face Mesh Detection - Triangulated Face Mapping  
+// Face Mesh Detection with ml5.js  
 // https://thecodingtrain.com/tracks/ml5js-beginners-guide/ml5/facemesh  
 // https://youtu.be/R5UZsIwPbJA  
 
 let video;
 let faceMesh;
 let faces = [];
-let triangles;
 
 function preload() {
-  // Load FaceMesh model 
+  // Initialize FaceMesh model with a maximum of one face and flipped video input
   faceMesh = ml5.faceMesh({ maxFaces: 1, flipped: true });
 }
 
 function mousePressed() {
+  // Log detected face data tothe console
   console.log(faces);
 }
 
@@ -27,55 +27,31 @@ function setup() {
 
   // Start detecting faces
   faceMesh.detectStart(video, gotFaces);
-
-  // Get predefined triangle connections
-  triangles = faceMesh.getTriangles();
 }
 
 function draw() {
   background(0);
-  video.loadPixels();
+  image(video, 0, 0);
 
+  // Ensure at least one face is detected
   if (faces.length > 0) {
     let face = faces[0];
 
-    randomSeed(5);
-    beginShape(TRIANGLES);
-    
-    // Loop through each triangle and fill it with sampled pixel color
-    for (let i = 0; i < triangles.length; i++) {
-      let tri = triangles[i];
-      let [a, b, c] = tri;
-      let pointA = face.keypoints[a];
-      let pointB = face.keypoints[b];
-      let pointC = face.keypoints[c];
-
-      // Calculate the centroid of the triangle
-      let cx = (pointA.x + pointB.x + pointC.x) / 3;
-      let cy = (pointA.y + pointB.y + pointC.y) / 3;
-
-      // Get color from video pixels at centroid location
-      let index = (floor(cx) + floor(cy) * video.width) * 4;
-      let rr = video.pixels[index];
-      let gg = video.pixels[index + 1];
-      let bb = video.pixels[index + 2];
-
+    // 畫出所有關鍵點（可移除這段如果只想要黑線與多邊形）
+    for (let i = 0; i < face.keypoints.length; i++) {
+      let keypoint = face.keypoints[i];
       stroke(255, 255, 0);
-      fill(rr, gg, bb);
-      vertex(pointA.x, pointA.y);
-      vertex(pointB.x, pointB.y);
-      vertex(pointC.x, pointC.y);
+      strokeWeight(2);
+      point(keypoint.x, keypoint.y);
     }
-    
-    endShape();
 
-    // ====== 你要的黑線與黑色多邊形 ======
+    // ====== 黑色線與黑色多邊形 ======
     stroke(0);
     strokeWeight(2);
     noFill();
 
-    // 1. 136,297,300 連線
-    let p1 = face.keypoints[136];
+    // 1. 336,297,300 連線
+    let p1 = face.keypoints[336];
     let p2 = face.keypoints[297];
     let p3 = face.keypoints[300];
     beginShape();
@@ -94,8 +70,8 @@ function draw() {
     vertex(p6.x, p6.y);
     endShape();
 
-    // 3. 238,241,125,19,354,461,456,250,462,370,94,141,242,20 連成多邊形並塗黑
-    let polyIdx = [238,241,125,19,354,461,456,250,462,370,94,141,242,20];
+    // 3. 238,241,125,19,354,461,458,250,462,370,94,141,242,20 連成多邊形並塗黑
+    let polyIdx = [238,241,125,19,354,461,458,250,462,370,94,141,242,20];
     fill(0);
     beginShape();
     for (let i = 0; i < polyIdx.length; i++) {
@@ -103,5 +79,6 @@ function draw() {
       vertex(pt.x, pt.y);
     }
     endShape(CLOSE);
+    noFill();
   }
 }
